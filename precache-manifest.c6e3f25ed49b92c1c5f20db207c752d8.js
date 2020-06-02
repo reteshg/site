5d6faf4a1e50e88e788c3f8159db2edc3322b136
reteshgondal@gmail.com <reteshgondal@gmail.com>
2020-06-02 04:17:13 -0400

    Updates

---
+++ precache-manifest.c6e3f25ed49b92c1c5f20db207c752d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27291715f83923173f2bb8946b2b761",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "56739fa4db4fe7c140df",
+    "url": "/site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0a0878b00732cb4a7d08",
+    "url": "/site/static/js/2.b485ff8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/site/static/js/2.b485ff8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56739fa4db4fe7c140df",
+    "url": "/site/static/js/main.9b1abb3c.chunk.js"
+  },
+  {
+    "revision": "0bdf1c9971b724945ab4",
+    "url": "/site/static/js/runtime-main.8737767f.js"
+  }
+]);
