fbd9dffa05efb1ea3f9de5cb10870b3519968e34
reteshgondal@gmail.com <reteshgondal@gmail.com>
2020-06-03 15:17:27 -0400

    ficxing navbar 2

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import {NavLink} from 'react-router-dom';
 
 const Navigation = (props) => (
 
   <Navbar fixed="top" bg="light">
     <Navbar.Brand>{props.title}</Navbar.Brand>
       <Nav className="mr-auto">
-        <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link href="/features">Features</Nav.Link>
-        <Nav.Link href="/faqs">Faqs</Nav.Link>
+        <Nav.Link><NavLink exact to="/">Home</NavLink></Nav.Link>
+        <Nav.Link><NavLink to="/features">Features</NavLink></Nav.Link>
+        <Nav.Link><NavLink to="/faqs">Faqs</NavLink></Nav.Link>
+        
       </Nav>
   </Navbar>
 
       /*
-      <Nav.Link><NavLink exact to="/">Home</NavLink></Nav.Link>
-        <Nav.Link><NavLink to="/features">Features</NavLink></Nav.Link>
-        <Nav.Link><NavLink to="/faqs">Faqs</NavLink></Nav.Link>
       
       
+      <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/features">Features</Nav.Link>
+        <Nav.Link href="/faqs">Faqs</Nav.Link>
       
       <nav>
         <h2>{props.title}</h2>
