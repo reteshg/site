aeeb462a43e5ab48b9a7bcc15600be1c874768c5
reteshgondal@gmail.com <reteshgondal@gmail.com>
2020-06-03 07:17:07 -0400

    add features file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/navbar';
 import Home from './components/home';
+import Features from './components/features';
 
 function App() {
   return (
     <div className="App">
       <Navbar title="The Timelapse App"/>
       <Home title="Homepage New"/>
+      <Features/>
     </div>
   );
 }
