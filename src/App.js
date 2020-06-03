e2bc80617d728df534a248160766aa12bc7e9e00
reteshgondal@gmail.com <reteshgondal@gmail.com>
2020-06-03 07:24:43 -0400

    browser router component added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import {BorwserRouter, BrowserRouter} from 'react-router-dom';
 import Navbar from './components/navbar';
 import Home from './components/home';
 import Features from './components/features';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar title="The Timelapse App"/>
-      <Home title="Homepage New"/>
-      <Features/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar title="The Timelapse App"/>
+        <Home title="Homepage New"/>
+        <Features/>
+      </div>
+    </BrowserRouter>
   );
 }
 
