e34c7a1da01f83c4bba0b43b8e040236d135e2e1
reteshgondal@gmail.com <reteshgondal@gmail.com>
2020-06-03 05:39:33 -0400

    added sample navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>test site</h1>
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => (
+      <nav>
+        <h2>The Timelapse App</h2>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Features</a></li>
+          <li><a href="#">FAQs</a></li>
+        </ul>
+      </nav>
+
+);
+
+export default Navbar;
