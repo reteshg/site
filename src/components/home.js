1186f647a0c087d7b4ff7b4e0de636a5ff6d8c79
reteshgondal@gmail.com <reteshgondal@gmail.com>
2020-06-09 12:25:25 -0400

    added video on homepage

--- src/components/home.js
+++ src/components/home.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import {Container,Row} from 'react-bootstrap';
+import './home.css';
 
 const Home = (props) => (
-   
-
-<Container fluid>
-
-
-<Row className="justify-content-center">
-<div style={{position: 'absolute', left: '50%', top: '50%',transform: 'translate(-50%, -50%)'}}>
-ss
-</div>
-
-</Row>
-
-
-        </Container>
+    
+    <Container fluid>
+        <Row className="justify-content-center">
+            <div className="centerAlignDiv">
+                <video width="100%"  preload="true" autoPlay muted loop="loop">
+                    <source src={process.env.PUBLIC_URL +'/assets/videos/output.mp4'} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+        </Row>
+    </Container>
 );
 
 export default Home;
